3bccf203cae8f9221e7c3ee8a7090d5c41cbb368
suryahadipranata20-ai <suryahadipranata20@gmail.com>
2026-05-18 11:51:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,4 @@ import streamlit as st
 
 st.title("Menentukan Indeks Kualitas Udara")
 st.header("Indeks Kualitas Udara (IKU)")
-st.write("merupakan ukuran komposit atau angka tunggal yang menggambarkan tingkat pencemaran atau status mutu udara di suatu lokasi pada waktu tertentu.
-Angka ini dihitung berdasarkan konsentrasi berbagai polutan utama seperti partikel debu, Sulfur Dioksida, Karbon Monoksida , Nitrogen Dioksida , dan Ozon.")
+st.write("merupakan ukuran komposit atau angka tunggal yang menggambarkan tingkat pencemaran atau status mutu udara di suatu lokasi pada waktu tertentu.Angka ini dihitung berdasarkan konsentrasi berbagai polutan utama seperti partikel debu, Sulfur Dioksida, Karbon Monoksida, Nitrogen Dioksida , dan Ozon.")
