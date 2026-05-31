8fa5826bdea735faef2354a6ae11c031415c5668
suryahadipranata20-ai <suryahadipranata20@gmail.com>
2026-05-31 17:58:42 +0700

    Update image URL in show_home function

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 def show_home():
     # Menampilkan gambar udara ambien
     st.image(
-        "https://images.unsplash.com/photo-1534274867516-169f9550a1d2?q=80&w=1000&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1551818255-e6e10975bc17?q=80&w=1000&auto=format&fit=crop",
         use_container_width=True
     )
     
