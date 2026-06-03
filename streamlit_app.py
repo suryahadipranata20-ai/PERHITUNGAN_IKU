6b988a6087b7640be9a494fdb86fb0a08ed967dc
suryahadipranata20-ai <suryahadipranata20@gmail.com>
2026-06-03 16:53:09 +0700

    Update image source in show_home function

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,9 @@ st.set_page_config(
 # --- Fungsi Halaman Utama (Home) ---
 def show_home():
     # Menampilkan gambar udara ambien
-    st.image("industrial.jpg", caption="Ilustrasi Polusi Udara Hasil Industri")
+    st.image(
+        "https://images.unsplash.com/photo-1569163139599-0f4517e36f51?q=80&w=1000&auto=format&fit=crop"
+    )
     
     st.markdown("<h1 style='text-align: center; color: #2E86AB;'>Perhitungan Indeks Kualitas Udara (IKU)</h1>", unsafe_allow_html=True)
     st.markdown("---")
