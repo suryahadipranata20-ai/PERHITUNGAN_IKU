922ba73b3143c0ee8cefa4e9b168530b7263a6bf
suryahadipranata20-ai <suryahadipranata20@gmail.com>
2026-06-03 17:26:55 +0700

    Refactor image display code for clarity

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,11 @@ st.set_page_config(
 # --- Fungsi Halaman Utama (Home) ---
 def show_home():
     # Menampilkan gambar udara ambien
-    st.image("polutan_industri.jpg", caption="Ilustrasi polutan yang dikeluarkan oleh industri",
-        use_container_width=True")
+    st.image(
+        "polutan_industri.jpg",  # simpan gambar pabrik berasap dengan nama ini
+        caption="Ilustrasi polutan yang dikeluarkan oleh industri",
+        use_container_width=True
+    ")
     
     st.markdown("<h1 style='text-align: center; color: #00AA00;'>Perhitungan Indeks Kualitas Udara (IKU)</h1>", unsafe_allow_html=True)
     st.markdown("---")
